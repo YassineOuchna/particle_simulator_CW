7383e77c4fd7019df62c498e0491d06b403f1269
Yassine Ouchna <yassine.ouchna@student-cs.fr>
2024-01-15 15:38:49 +0100

    buttons and sliders

--- Particle_Simulator.py
+++ Particle_Simulator.py
@@ -6,7 +6,7 @@ HEIGHT = 750
 RUNNING = True
 FONT_SIZE = 40
 XPLACEMENT = int(0.5*WIDTH)
-YPLACEMENT = int(0.025*HEIGHT)
+YPLACEMENT = int(0.05*HEIGHT)
 BH = int(HEIGHT*0.05)
 BW = int(WIDTH*0.15)
 # Initializing display
@@ -17,39 +17,64 @@ pygame.display.set_caption('Particle Simulator')
 # Setting up first screen
 
 
+def wave_button(x, y, w, h):
+    l = pygame.draw.rect(screen, (10, 0, 255),
+                         (x-w//2, y-h//2, w, h))
+    font = pygame.font.Font(None, 36)
+    text = font.render("Wave Propagation", True, (255, 255, 255))
+    text_rect = text.get_rect(center=(x, y))
+    screen.blit(text, text_rect)
+    return l
+
+
+def diffusion_button(x, y, w, h):
+    l = pygame.draw.rect(screen, (10, 0, 255),
+                         (x-w//2, y-h//2, w, h))
+    font = pygame.font.Font(None, 36)
+    text = font.render("Diffusion", True, (255, 255, 255))
+    text_rect = text.get_rect(center=(x, y))
+    screen.blit(text, text_rect)
+    return l
+
+
+def quit_button(x, y, w, h):
+    l = pygame.draw.rect(screen, (10, 0, 255),
+                         (x-w//2, y-h//2, w, h))
+    font = pygame.font.Font(None, 36)
+    text = font.render("Quit", True, (255, 255, 255))
+    text_rect = text.get_rect(center=(x, y))
+    screen.blit(text, text_rect)
+    return l
+
+
 def main_menu():
     font = pygame.font.Font(None, FONT_SIZE)
     text = font.render('Particle Simulator', True, (255, 255, 255))
     text_rect = text.get_rect(center=(XPLACEMENT, YPLACEMENT))
     screen.blit(text, text_rect)
+    a = wave_button(XPLACEMENT, int(HEIGHT*0.35), 250, 35)
+    b = diffusion_button(XPLACEMENT, int(HEIGHT*0.35)+70, 250, 35)
+    c = quit_button(XPLACEMENT, int(HEIGHT*0.35)+140, 250, 35)
+    return a, b, c
 
 
-def wave_button(x, y, w, h):
-    pygame.draw.rect(screen, (10, 0, 255),
-                     (x-w//2, y-h//2, w, h))
-    font = pygame.font.Font(None, 36)
-    text = font.render("Wave mode", True, (255, 255, 255))
-    text_rect = text.get_rect(center=(x, y))
-    screen.blit(text, text_rect)
-
-
-def diffusion_button(x, y, w, h):
-    pygame.draw.rect(screen, (10, 0, 255),
-                     (x-w//2, y-h//2, w, h))
-    font = pygame.font.Font(None, 36)
-    text = font.render("Diffusion", True, (255, 255, 255))
-    text_rect = text.get_rect(center=(x, y))
-    screen.blit(text, text_rect)
-
-
-main_menu()
-wave_button(XPLACEMENT, int(HEIGHT*0.35), 150, 50)
-diffusion_button(XPLACEMENT, int(HEIGHT*0.35)+60, 150, 50)
+WaveButton, DiffusionButton, QuitButton = main_menu()
 while RUNNING:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             RUNNING = False
 
-    pygame.display.flip()
+        # Button management
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:     # Left mouse click
+                mx, my = pygame.mouse.get_pos()
+                if WaveButton.collidepoint(mx, my):
+                    pygame.display.set_mode((500, 500))
+                if DiffusionButton.collidepoint(mx, my):
+                    print('Diffuse')
+                if QuitButton.collidepoint(mx, my):
+                    RUNNING = False
+
+    pygame.display.update()
 
 pygame.quit()
---
+++ wave_2.py
@@ -0,0 +1,109 @@
+import pygame
+import pymunk
+import random
+import time
+
+'''--- INITIALIZING ENVIRONEMENT & GLOBAL CONSTANTS ---'''
+
+pygame.init()
+space = pymunk.Space()
+WIDTH, HEIGHT = int(
+    0.9*pygame.display.Info().current_w), int(0.9*pygame.display.Info().current_h)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+w_slider = int(0.25*WIDTH)
+h_slider = int(0.035*HEIGHT)
+
+
+'''--- GUI ELEMENTS ---'''
+
+
+class Slider():
+    def __init__(self, title, min, max, x, y):
+        self.title = title
+        self.x = x            # Position of the topleft of the slider
+        self.y = y
+        self.min = min
+        self.max = max
+        self.value = min
+
+    def render(self):
+        box = pygame.draw.rect(screen, (134, 136, 138),
+                               (self.x, self.y, w_slider, h_slider))
+        c_pos = int(((self.value-self.min)*w_slider) /
+                    (self.max-self.min)+self.x-(0.1*w_slider)//2)
+        cursor = pygame.draw.rect(
+            screen, (202, 204, 206), (c_pos, self.y, int(0.1*w_slider), h_slider))
+
+        font = pygame.font.Font(None, 36)
+        title = font.render(self.title, True, (255, 255, 255))
+        value = font.render(str(self.value), True, (255, 255, 255))
+        title_rect = title.get_rect(center=(self.x-125, self.y+h_slider//2))
+        value_rect = value.get_rect(
+            center=(c_pos+0.1*w_slider//2, self.y-h_slider//2))
+        screen.blit(title, title_rect)
+        screen.blit(value, value_rect)
+        return cursor
+
+
+l = Slider("TEST", 0, 5, WIDTH//2, HEIGHT//2)
+
+'''--- SIMULATION ELEMENTS ---
+
+class Ball():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.body = pymunk.Body()
+        self.body.position = x, y
+        # 100 in velocity means 100 frames per sec
+        # random float nunber
+        self.body.velocity = random.uniform(-speed_limit,
+                                            speed_limit), random.uniform(-speed_limit, speed_limit)
+        self.shape = pymunk.Circle(self.body, particle_size)
+        self.shape.density = 1
+        self.shape.elasticity = elasticity
+
+        space.add(self.body, self.shape)
+
+class Satistic_Wall():
+    def __init__(self, p1, p2):
+        self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        # I don't quite understand it here
+        self.shape = pymunk.Segment(self.body, p1, p2, 3)
+        self.shape.elasticity = 1
+        space.add(self.shape, self.body)
+
+class Kinematic_Wall():
+    def __init__(self, p1, p2):
+        self.body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+        self.shape = pymunk.Segment(self.body, p1, p2, 3)
+        self.shape.body.velocity = (0, 0)
+        self.shape.elasticity = pusher_elasticity
+        self.time = 0
+        space.add(self.shape, self.body)
+
+    def stop(self):
+        self.time = self.time+1
+        if self.time < pusher_start_time:
+            self.shape.body.velocity = (0, 0)
+        elif self.time > pusher_run_time+pusher_start_time:
+            self.shape.body.velocity = (0, 0)
+        else:
+            self.shape.body.velocity = (pusher_speed, 0)'''
+
+
+if __name__ == "__main__":
+    RUNNING = True
+    while RUNNING:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                RUNNING = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed()[0]:     # Left mouse click
+                    mx, my = pygame.mouse.get_pos()
+                    if l.render().collidepoint(mx, my):
+                        l.value = 1.5
+        screen.fill((0, 0, 0))
+        l.render()
+        pygame.display.update()
+    pygame.quit()
